6c25e48b1f4f5eee5c270d5c3868209bf70e439b
Kongou Hikari <hikari@iloli.bid>
2019-11-29 14:55:57 +0800

    Fix

--- access.c
+++ access.c
@@ -23,33 +23,40 @@ vsf_access_check_file(const struct mystr* p_filename_str)
   {
     return 1;
   }
+  if (tunable_deny_file)
+  {
+    if (str_isempty(&s_access_str))
+    {
+      str_alloc_text(&s_access_str, tunable_deny_file);
+    }
+    if (vsf_filename_passes_filter(p_filename_str, &s_access_str, &iters))
+    {
+      return 0;
+    }
+    else
+    {
+      struct str_locate_result loc_res =
+        str_locate_str(p_filename_str, &s_access_str);
+      if (loc_res.found)
+      {
+        return 0;
+      }
+    }
+  }
 
-  if (str_isempty(&s_access_str))
+  if (tunable_allow_prefix)
   {
-    str_alloc_text(&s_access_str, tunable_deny_file);
-  }
-  if (str_isempty(&s_prefix_str))
-  {
-    str_alloc_text(&s_prefix_str, tunable_allow_prefix);
-  }
-  if (vsf_filename_passes_filter(p_filename_str, &s_prefix_str, &iters) == 0)
-  {
-    return 0;
-  }
-  iters = 0;
-  if (vsf_filename_passes_filter(p_filename_str, &s_access_str, &iters))
-  {
-    return 0;
-  }
-  else
-  {
-    struct str_locate_result loc_res =
-      str_locate_str(p_filename_str, &s_access_str);
-    if (loc_res.found)
+    if (str_isempty(&s_prefix_str))
+    {
+      str_alloc_text(&s_prefix_str, tunable_allow_prefix);
+    }
+    iters = 0;
+    if (vsf_filename_passes_filter(p_filename_str, &s_prefix_str, &iters) == 0)
     {
       return 0;
     }
   }
+
   return 1;
 }
 
@@ -60,37 +67,44 @@ vsf_access_check_file_visible(const struct mystr* p_filename_str)
   static struct mystr s_prefix_str;
   unsigned int iters = 0;
 
-  if (!tunable_deny_file && !tunable_allow_prefix)
+  if (!tunable_hide_file && !tunable_allow_prefix)
   {
     return 1;
   }
+  if (tunable_hide_file)
+  {
+    if (str_isempty(&s_access_str))
+    {
+      str_alloc_text(&s_access_str, tunable_hide_file);
+    }
+    if (vsf_filename_passes_filter(p_filename_str, &s_access_str, &iters))
+    {
+      return 0;
+    }
+    else
+    {
+      struct str_locate_result loc_res =
+        str_locate_str(p_filename_str, &s_access_str);
+      if (loc_res.found)
+      {
+        return 0;
+      }
+    }
+  }
 
-  if (str_isempty(&s_access_str))
+  if (tunable_allow_prefix)
   {
-    str_alloc_text(&s_access_str, tunable_hide_file);
-  }
-  if (str_isempty(&s_prefix_str))
-  {
-    str_alloc_text(&s_prefix_str, tunable_allow_prefix);
-  }
-  if (vsf_filename_passes_filter(p_filename_str, &s_prefix_str, &iters) == 0)
-  {
-    return 0;
-  }
-  iters = 0;
-  if (vsf_filename_passes_filter(p_filename_str, &s_access_str, &iters))
-  {
-    return 0;
-  }
-  else
-  {
-    struct str_locate_result loc_res =
-      str_locate_str(p_filename_str, &s_access_str);
-    if (loc_res.found)
+    if (str_isempty(&s_prefix_str))
+    {
+      str_alloc_text(&s_prefix_str, tunable_allow_prefix);
+    }
+    iters = 0;
+    if (vsf_filename_passes_filter(p_filename_str, &s_prefix_str, &iters) == 0)
     {
       return 0;
     }
   }
+  
   return 1;
 }
 
